6610d13b7897238eaca562bdef72286e00a677ec
Keerath Jaggi <keerath.jaggi@adp.com>
2020-04-25 00:19:05 +0530

    cosmetic changes

--- src/main/java/com/google/android/AppDetails.java
+++ src/main/java/com/google/android/AppDetails.java
@@ -6,6 +6,7 @@ import java.util.Date;
 
 public class AppDetails {
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MMM dd, yyyy");
+    private static final int MILLI_SECS_IN_DAY = 24 * 60 * 60 * 1000;
 
     private final String appName;
     private final long numReviews;
@@ -22,14 +23,16 @@ public class AppDetails {
             e.printStackTrace();
             this.lastUpdated = null;
         }
-        this.daysSinceLastUpdate = Math.toIntExact((new Date().getTime() - this.lastUpdated.getTime()) / (24 * 60 * 60 * 1000));
+        assert this.lastUpdated != null;
+        this.daysSinceLastUpdate = Math.toIntExact((new Date().getTime() - this.lastUpdated.getTime()) / MILLI_SECS_IN_DAY);
         this.score = this.numReviews / (this.daysSinceLastUpdate * 1.0f);
     }
 
     @Override
     public String toString() {
-        return String.join("\t", appName, String.valueOf(numReviews),
-                DATE_FORMAT.format(lastUpdated), String.valueOf(daysSinceLastUpdate),
-                String.valueOf(score)) + System.lineSeparator();
+        return String.join("\t",
+                appName, String.valueOf(numReviews), DATE_FORMAT.format(lastUpdated),
+                String.valueOf(daysSinceLastUpdate), String.valueOf(score)
+        ) + System.lineSeparator();
     }
 }
--- src/main/java/com/google/android/Main.java
+++ src/main/java/com/google/android/Main.java
@@ -20,24 +20,36 @@ public class Main {
     private static final String TOP_CHARTS_URL = GOOGLE_PLAY_URL + "/store/apps/top";
     private static final Path APP_DETAILS_OUTPUT_PATH =
             Paths.get(System.getProperty("user.home") + "/playstore-top-app-details.tsv");
-    private static final String HEADER = String.join("\t",
-            "APP_NAME", "NUM_REVIEWS", "LAST_UPDATED", "DAYS_SINCE_LAST_UPDATE", "SCORE") +
-            System.lineSeparator();
+
+    private static final byte[] HEADER = (
+            String.join("\t",
+                    "APP_NAME", "NUM_REVIEWS", "LAST_UPDATED", "DAYS_SINCE_LAST_UPDATE", "SCORE") +
+                    System.lineSeparator()
+    ).getBytes();
+
+    private static final String APP_DIV_SELECTOR = "div.b8cIId.ReQCgd.Q9MA7b";
+    private static final String APP_NAME_SELECTOR = "div.WsMG1c.nnK0zc";
+    private static final String APP_LINK_SELECTOR = "a";
+    private static final String APP_NUM_REVIEWS_SELECTOR = "span.AYi5wd.TBRnV span";
+    private static final String APP_UPDATED_DIV_SELECTOR = "div.hAyfc";
+    private static final String APP_UPDATED_SELECTOR = "span.htlgb span.htlgb";
 
     public static void main(String[] args) throws IOException {
         // clean up any existing file
         Files.deleteIfExists(APP_DETAILS_OUTPUT_PATH);
         Files.createFile(APP_DETAILS_OUTPUT_PATH);
-        Files.write(APP_DETAILS_OUTPUT_PATH, HEADER.getBytes(), StandardOpenOption.APPEND);
+        Files.write(APP_DETAILS_OUTPUT_PATH, HEADER, StandardOpenOption.APPEND);
+
         // parse app name and its link from top apps page
         Map<String, String> topAppsNameVsLink = parseTopAppsNameVsLink();
+
         for (Map.Entry<String, String> appNameVsLink : topAppsNameVsLink.entrySet()) {
             // parse app details for each app
             AppDetails appDetails = parseAppDetails(appNameVsLink.getKey(), appNameVsLink.getValue());
+            // write app details for each app as tsv (check toString)
             Files.write(APP_DETAILS_OUTPUT_PATH, appDetails.toString().getBytes(), StandardOpenOption.APPEND);
         }
     }
-
 
     private static Map<String, String> parseTopAppsNameVsLink() {
         Document doc = null;
@@ -48,11 +60,11 @@ public class Main {
         }
         assert doc != null;
 
-        Elements appDivs = doc.select("div.b8cIId.ReQCgd.Q9MA7b");
+        Elements appDivs = doc.select(APP_DIV_SELECTOR);
         Map<String, String> appNameVsLink = new HashMap<>();
         for (Element appDiv : appDivs) {
-            String appName = appDiv.selectFirst("div.WsMG1c.nnK0zc").attr("title");
-            String appLink = appDiv.selectFirst("a").attr("href");
+            String appName = appDiv.selectFirst(APP_NAME_SELECTOR).attr("title");
+            String appLink = appDiv.selectFirst(APP_LINK_SELECTOR).attr("href");
             appNameVsLink.put(appName, appLink);
         }
         return appNameVsLink;
@@ -67,17 +79,12 @@ public class Main {
         }
         assert doc != null;
 
-        String numReviews = doc.selectFirst("span.AYi5wd.TBRnV span").text();
-        Optional<Element> updatedDiv = doc.select("div.hAyfc").stream()
+        String numReviews = doc.selectFirst(APP_NUM_REVIEWS_SELECTOR).text();
+        Optional<Element> updatedDiv = doc.select(APP_UPDATED_DIV_SELECTOR).stream()
                 .filter(elem -> elem.text().contains("Updated"))
                 .findFirst();
-
-        String lastUpdated = null;
-        if (updatedDiv.isPresent()) {
-            lastUpdated = updatedDiv.get().select("span.htlgb span.htlgb").text();
-        }
-        assert lastUpdated != null;
+        assert updatedDiv.isPresent();
+        String lastUpdated = updatedDiv.get().select(APP_UPDATED_SELECTOR).text();
         return new AppDetails(appName, numReviews, lastUpdated);
     }
-
 }
